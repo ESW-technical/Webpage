5b9d1905946c998c68725e83342621d6a0b9e16d
saina-s <95881045+saina-s@users.noreply.github.com>
2024-07-24 20:31:58 +0300

    Add files via upload

---
+++ final-ESW/js/script.js
@@ -0,0 +1,69 @@
+let menubtn = document.querySelector('#menu-btn');
+let navbar =  document.querySelector('.header .flex .navbar');
+
+menubtn.onclick = () =>{
+    menubtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+// countdown1
+var countDownDate = new Date("August 2, 2024 00:00:01").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown1").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown1").innerHTML = "Registration is over!";
+    document.getElementById("countdown-comment1").innerHTML = "";
+  }
+}, 1000);
+
+
+//countdown2
+var countDownDate2 = new Date("August 16, 2024 00:00:01").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate2 - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown2").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown2").innerHTML = "Registration is over!";
+    document.getElementById("countdown-comment2").innerHTML = "";
+  }
+}, 1000);
+
